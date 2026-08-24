c8583f063ecad0b532b084a9efb1411f6c2c142b
okcricknew <okcrick2@gmail.com>
2026-08-24 22:31:42 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 import MarketListClient from "../components/MarketListClient";
 import { getInitialMarketResults } from "../services/marketService";
+import PhoneLoginClient from "../components/PhoneLoginClient";
 
 export default async function HomePage() {
   const initialResults = await getInitialMarketResults();
 
   return (
     <main>
+    <PhoneLoginClient />
       <MarketListClient
         initialResults={initialResults}
       />
