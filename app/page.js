00a0d9bba3ec0b3660a878ee1976bd935ba856f1
okcricknew <okcrick2@gmail.com>
2026-08-24 22:16:46 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 import MarketListClient from "../components/MarketListClient";
 import { getInitialMarketResults } from "../services/marketService";
 
