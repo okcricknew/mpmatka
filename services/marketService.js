7e0c39fb7f72d841c76158fa3e1868da03ef847d
okcricknew <okcrick2@gmail.com>
2026-08-24 15:39:26 +0530

    Update marketService.js

--- services/marketService.js
+++ services/marketService.js
@@ -1,18 +1,7 @@
 import { db } from "../lib/firebase-admin";
+import { STATIC_MARKETS } from "../utils/constants";
 
-export const STATIC_MARKETS = [
-  { id: 3, name: 'SRIDEVI', time: '11:35 AM - 12:35 PM' },
-  { id: 4, name: 'TIME BAZAR', time: '01:00 PM - 02:00 PM' },
-  { id: 7, name: 'MILAN DAY', time: '03:00 PM - 05:00 PM' },
-  { id: 8, name: 'RAJDHANI DAY', time: '03:00 PM - 05:00 PM' },
-  { id: 9, name: 'SUPREME DAY', time: '03:35 PM - 05:35 PM' },
-  { id: 6, name: 'KALYAN', time: '03:45 PM - 05:45 PM' },
-  { id: 10, name: 'SRIDEVI NIGHT', time: '07:15 PM - 08:15 PM' },
-  { id: 5, name: 'MILAN NIGHT', time: '09:10 PM - 11:10 PM' },
-  { id: 2, name: 'KALYAN NIGHT', time: '9:45 PM - 11:45 PM' },
-  { id: 1, name: 'RAJDHANI NIGHT', time: '09:30 PM - 11:45 PM' },
-  { id: 11, name: 'MAIN BAZAR', time: '09:35 PM - 12:05 AM' },
-];
+export { STATIC_MARKETS };
 
 export async function getInitialMarketResults() {
   try {
@@ -43,4 +32,3 @@ export async function getInitialMarketResults() {
     return fallbackMap;
   }
 }
-
