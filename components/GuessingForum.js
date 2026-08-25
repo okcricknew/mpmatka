fb3e49bfb020dbda82238375d6d23c3aae23a683
okcricknew <okcrick2@gmail.com>
2026-08-25 20:05:18 +0530

    Update GuessingForum.js

--- components/GuessingForum.js
+++ components/GuessingForum.js
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect, useRef } from 'react'
-import { db, auth } from '../firebase'
+import { db, auth } from '../lib/firebase'
 import { collection, onSnapshot, addDoc, serverTimestamp, doc, getDoc, deleteDoc } from 'firebase/firestore'
 import { onAuthStateChanged } from 'firebase/auth'
 
