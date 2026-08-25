7c666962bfb67c71c6188a5e51fad96fc3a1d528
okcricknew <okcrick2@gmail.com>
2026-08-25 08:15:36 +0530

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,10 @@
+import LoginClient from "../../components/LoginClient";
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-10">
+      <LoginClient />
+    </main>
+  );
+}
+
