c64017a4bf47954d294077f0f1816bb160bbb9c1
okcricknew <okcrick2@gmail.com>
2026-08-25 10:22:44 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import Link from 'next/link';
 import MarketListClient from "../components/MarketListClient";
 import { getInitialMarketResults } from "../services/marketService";
 import UserLoginRegister from "../components/UserLoginRegister";
@@ -10,9 +9,9 @@ export default async function HomePage() {
   const initialResults = await getInitialMarketResults();
 
   return (
-    <main className="w-full bg-[#f5f7fb] pb-10 px-1">
+    <main className="w-full max-w-none min-w-0 bg-[#f5f7fb] pb-10 px-1 m-0">
       {/* Market Results List & User Login Section */}
-      <div className="w-full px-0 mt-2">
+      <div className="w-full max-w-none min-w-0 m-0 mt-2 p-0">
         <MarketListClient initialResults={initialResults} />
         <UserLoginRegister />
       </div>
