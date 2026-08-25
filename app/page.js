b144e8c5500e87480211a20af4b99b050c249b63
okcricknew <okcrick2@gmail.com>
2026-08-25 15:03:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export const revalidate = 0;
 import MarketListClient from "../components/MarketListClient";
 import { getInitialMarketResults } from "../services/marketService";
 import UserLoginRegister from "../components/UserLoginRegister";
+import LiveResult from "../components/LiveResult";
 
 export default async function HomePage() {
   const initialResults = await getInitialMarketResults();
@@ -12,6 +13,7 @@ export default async function HomePage() {
     <main className="w-full max-w-none min-w-0 bg-[#f5f7fb] pb-10 px-1 m-0">
       {/* Market Results List & User Login Section */}
       <div className="w-full max-w-none min-w-0 m-0 mt-2 p-0">
+    <LiveResult />
         <MarketListClient initialResults={initialResults} />
         <UserLoginRegister />
       </div>
