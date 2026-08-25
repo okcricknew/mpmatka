6687880e8eaf9e7a01af22e098ec76c428e35e24
okcricknew <okcrick2@gmail.com>
2026-08-25 20:30:44 +0530

    Update page.js

--- app/guessing-forum/page.js
+++ app/guessing-forum/page.js
@@ -1,6 +1,17 @@
-import GuessingForum from '../../components/GuessingForum';
+'use client'
+
+import dynamic from 'next/dynamic'
+
+// SSR ko disable karke direct browser (client) par load karne ke liye
+const GuessingForum = dynamic(() => import('../../components/GuessingForum'), {
+  ssr: false,
+  loading: () => (
+    <div className="text-center py-10 font-bold text-xs text-gray-600 bg-white">
+      Loading Guessing Forum...
+    </div>
+  ),
+})
 
 export default function Page() {
-  return <GuessingForum initialPosts={[]} />;
-    }
-  
+  return <GuessingForum initialPosts={[]} />
+}
