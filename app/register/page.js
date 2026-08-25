9146a169f704fbc7b9bda653be31595df34df1c0
okcricknew <okcrick2@gmail.com>
2026-08-25 07:51:00 +0530

    Create page.js

---
+++ app/register/page.js
@@ -0,0 +1,10 @@
+import RegisterClient from "../../components/RegisterClient";
+
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-10">
+      <RegisterClient />
+    </main>
+  );
+}
+
