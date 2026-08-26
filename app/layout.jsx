030306848e40cfea230213c3a2d5c2670f29eab7
okcricknew <okcrick2@gmail.com>
2026-08-26 21:18:16 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "../context/AuthContext";
 import "./globals.css";
 
 export const metadata = {
@@ -12,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthProvider>
+        {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
