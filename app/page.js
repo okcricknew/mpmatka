60280cc76012c9785d90cb3c23a0a1ccda4be532
okcricknew <okcrick2@gmail.com>
2026-08-27 16:16:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "../lib/auth-session";
+
 import MarketListClient from "../components/MarketListClient";
 import { getInitialMarketResults } from "../services/marketService";
 import UserLoginRegister from "../components/UserLoginRegister";
@@ -8,16 +11,22 @@ import LiveResults from "../components/LiveResults";
 import GamesAndChartsZone from "../components/GamesAndChartsZone";
 
 export default async function HomePage() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   const initialResults = await getInitialMarketResults();
 
   return (
     <main className="w-full max-w-none min-w-0 bg-[#f5f7fb] pb-10 px-1.5 sm:px-4 m-0">
       {/* Market Results List & User Login Section */}
       <div className="w-full max-w-none min-w-0 m-0 mt-2 p-0">
-    <LiveResults />
+        <LiveResults />
         <MarketListClient initialResults={initialResults} />
         <UserLoginRegister />
-    <GamesAndChartsZone />
+        <GamesAndChartsZone />
       </div>
     </main>
   );
