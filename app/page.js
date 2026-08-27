c25338d98f16ba4af5e55c5ef136cc8ca769dd6c
okcricknew <okcrick2@gmail.com>
2026-08-27 16:19:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default async function HomePage() {
   const user = await getCurrentUser();
 
   if (!user) {
-    redirect("/login");
+    redirect("/");
   }
 
   const initialResults = await getInitialMarketResults();
