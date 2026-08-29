d4f069b05baaa1fa43d67fcd1c76c64cbff1d07a
okcricknew <okcrick2@gmail.com>
2026-08-29 08:51:55 +0530

    Update page.js

--- app/guessing-forum/page.js
+++ app/guessing-forum/page.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function GuessingForumPage() {
   return (
-    <main className="w-full max-w-full bg-[#f5f7fb] pb-10 m-0 box-border">
+    <main className="w-full max-w-full bg-[#f5f7fb] pb-10 px-1 sm:px-4 m-0 box-border">
       <GuessingForum />
     </main>
   );
