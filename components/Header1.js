673c39f7cc88e6bef89172c13a4b93386d6582fb
okcricknew <okcrick2@gmail.com>
2026-08-30 19:48:51 +0530

    Create Header1.js

---
+++ components/Header1.js
@@ -0,0 +1,13 @@
+export default function Header1() {
+  return (
+    <div className="bg-[#e6ffe6] border-2 border-green-600 p-3 text-center my-2">
+      <h3 className="font-serif italic font-bold text-black text-base mb-1.5">
+        SattaMatka
+      </h3>
+      <p className="font-sans italic text-xs sm:text-sm text-black leading-relaxed m-0 font-normal">
+        Satta Matka * Sattamatka * New Mumbai Ratan Satta Matka * Fast Matka * Milan Market * Kalyan Matka Results * Satta Game * Matka Game * Satta Matka * Kalyan Satta Matka * Mumbai Main * Online Matka Results * Satta Matka Tips * Milan Chart * Satta Matka Boss* New Star Day * Satta King * Live Satta Matka Results * Satta Matka Company * Indian Matka * Satta Matka 143* Kalyan Night Matka..
+      </p>
+    </div>
+  );
+}
+
