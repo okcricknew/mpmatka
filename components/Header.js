2f8a5a8cf04fa7943db2000ed657dce8837871b7
okcricknew <okcrick2@gmail.com>
2026-08-30 19:42:54 +0530

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="text-center bg-white p-2.5 border-2 border-red-600">
+      <h1 className="font-sans font-bold text-[32px] m-0">
+        <span className="text-[#660066]">SATTA</span>
+        <span className="text-red-600">143</span>
+        <span className="text-[#660066]">.IN</span>
+      </h1>
+      <p className="font-sans font-bold text-[#003300] text-sm mt-[5px] mb-0">
+        Welcome to Satta Matka
+      </p>
+    </div>
+  );
+}
+
