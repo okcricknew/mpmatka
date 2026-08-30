019b9daea8823edf70839a99488ab5a243ea79af
okcricknew <okcrick2@gmail.com>
2026-08-30 09:46:31 +0530

    Create kalyanSeedData.js

---
+++ data/kalyanSeedData.js
@@ -0,0 +1,43 @@
+export const kalyanSeedData = [
+  {
+    startDate: '31/12/2012',
+    endDate: '05/01/2013',
+    weekData: {
+      mon: { openPanna: '799', jodi: '51', closePanna: '155' },
+      tue: { openPanna: '345', jodi: '29', closePanna: '360' },
+      wed: { openPanna: '339', jodi: '50', closePanna: '569' },
+      thu: { openPanna: '137', jodi: '18', closePanna: '468' },
+      fri: { openPanna: '369', jodi: '84', closePanna: '680' },
+      sat: { openPanna: '335', jodi: '17', closePanna: '359' },
+      sun: { openPanna: '', jodi: '', closePanna: '' }
+    }
+  },
+
+  {
+    startDate: '07/01/2013',
+    endDate: '12/01/2013',
+    weekData: {
+      mon: { openPanna: '478', jodi: '99', closePanna: '667' },
+      tue: { openPanna: '568', jodi: '96', closePanna: '150' },
+      wed: { openPanna: '157', jodi: '33', closePanna: '256' },
+      thu: { openPanna: '234', jodi: '96', closePanna: '259' },
+      fri: { openPanna: '349', jodi: '67', closePanna: '368' },
+      sat: { openPanna: '246', jodi: '26', closePanna: '330' },
+      sun: { openPanna: '', jodi: '', closePanna: '' }
+    }
+  },
+
+  {
+    startDate: '14/01/2013',
+    endDate: '19/01/2013',
+    weekData: {
+      mon: { openPanna: '***', jodi: '**', closePanna: '***' },
+      tue: { openPanna: '290', jodi: '17', closePanna: '890' },
+      wed: { openPanna: '567', jodi: '88', closePanna: '279' },
+      thu: { openPanna: '270', jodi: '99', closePanna: '234' },
+      fri: { openPanna: '480', jodi: '28', closePanna: '279' },
+      sat: { openPanna: '199', jodi: '96', closePanna: '277' },
+      sun: { openPanna: '', jodi: '', closePanna: '' }
+    }
+  }
+];
