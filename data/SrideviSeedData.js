0660a3a63422647a3fcd6bf8abe5ac12171e0490
okcricknew <okcrick2@gmail.com>
2026-09-02 07:53:53 +0530

    Create SrideviSeedData.js

---
+++ data/SrideviSeedData.js
@@ -0,0 +1,15 @@
+export const SrideviSeedData = [
+  {
+    startDate: '04/01/2021',
+    endDate: '10/01/2021',
+    weekData: {
+      mon: { openPanna: '459', jodi: '80', closePanna: '280' },
+      tue: { openPanna: '466', jodi: '64', closePanna: '770' },
+      wed: { openPanna: '229', jodi: '35', closePanna: '889' },
+      thu: { openPanna: '126', jodi: '92', closePanna: '480' },
+      fri: { openPanna: '247', jodi: '35', closePanna: '113' },
+      sat: { openPanna: '278', jodi: '72', closePanna: '345' },
+      sun: { openPanna: '567', jodi: '84', closePanna: '130' }
+    }
+  }
+];
